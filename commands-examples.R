4852e166fea9773ce1dad637a7f08d5cc8b29784
Robert Johnson <robertjohnson@Roberts-Mini.attlocal.net>
2019-12-27 03:41:18 -0600

    Getting started with basic commands list

---
+++ commands-examples.R
@@ -0,0 +1,23 @@
+#get a list and information on every dataset that comes with R
+#datasets is a package; this command gets you information on that package
+library(help = "datasets")
+
+# a slightly more succinct version - no package description
+data()
+
+
+# memory.limit() is a windows-specific command; won't work on Mac
+
+
+#Setting your working directory, 
+#from https://www.computerworld.com/article/2497143/business-intelligence-beginner-s-guide-to-r-introduction.html?page=3
+#Note that the slashes always have to be forward slashes, even if you're on a Windows system.
+#If you are using RStudio, you can also use the menu to change your working directory under Session > Set Working Directory.
+#For Windows, the command might look something like: setwd("C:/Sharon/Documents/RProjects")
+#Change your working directory with the setwd() function, such as:
+  
+#setwd("~/mydirectory")
+
+
+
+
